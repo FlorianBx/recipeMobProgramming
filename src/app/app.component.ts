2ee3619d01a6920042903498f39daf4fada48450
mohamed laadjal <laadjalmohamed@gmail.com>
2024-09-02 16:33:46 +0200

    feat: Appel au service + affichage recettes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { RecipeService } from './recipe.service';
+import { Recipe } from './models/recipe.model';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +13,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'recipeMob';
+  recipes: Recipe[] = [];
+
+constructor(public recipeService: RecipeService) {
+
+
 }
+
+ngOnInit() {
+  this.recipes = this.recipeService.getRecipes();
+  this.recipeService.getRecipes();
+  console.log(this.recipeService.getRecipes());
+
+}
+
+}
+
