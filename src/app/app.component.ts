870f2bef22e6e6f5b399f7caad1978565eb8eae8
Moezbouali <Moezbouali74@gmail.com>
2024-09-03 11:42:40 +0200

    ajout header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './components/header/header.component';
 import { RecipeService } from './recipe.service';
 import { Recipe } from './models/recipe.model';
 import { ListComponent } from './components/list/list.component';
@@ -9,7 +9,7 @@ import { DetailComponent } from './components/detail/detail.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, DetailComponent],
+  imports: [RouterOutlet, ListComponent, DetailComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
